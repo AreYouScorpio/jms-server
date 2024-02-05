17154b10cff81248069503e28d6cd60e080d96b6
AreYouScorpio <akos.schweger@gmail.com>
2024-02-05 20:36:40 +0100

    Initial commit

---
+++ src/main/java/hu/webuni/spring/jmsserver/config/JmsConfig.java
@@ -0,0 +1,26 @@
+package hu.webuni.spring.jmsserver.config;
+
+import org.apache.activemq.artemis.api.core.TransportConfiguration;
+import org.apache.activemq.artemis.core.remoting.impl.netty.NettyAcceptorFactory;
+import org.apache.activemq.artemis.core.remoting.impl.netty.NettyConnectorFactory;
+import org.springframework.boot.autoconfigure.jms.artemis.ArtemisConfigurationCustomizer;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
+
+@Configuration
+public class JmsConfig {
+
+    @Configuration
+    public class ArtemisConfig implements ArtemisConfigurationCustomizer {
+        @Override
+        public void customize(org.apache.activemq.artemis.core.config.Configuration configuration) {
+            //params elfogad egy Map-et is ^^
+            Map<String, Object> params = Map.of(
+                    "host", "localhost",
+                    "port", 61617);
+            configuration.addConnectorConfiguration("nettyConnector", new TransportConfiguration(NettyConnectorFactory.class.getName()));
+            configuration.addAcceptorConfiguration(new TransportConfiguration(NettyAcceptorFactory.class.getName(), params));
+        }
+    }
+}
---
+++ src/test/java/hu/webuni/spring/jmsserver/JmsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.webuni.spring.jmsserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmsServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
